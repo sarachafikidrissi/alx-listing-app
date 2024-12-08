0509e35b792601ac0d15b1171a64331ab5200b4f
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-12-08 15:53:19 +0100

    update folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "../app/styles/globals.css";
+import "./styles/globals.css";
 
 
 const geistSans = localFont({
